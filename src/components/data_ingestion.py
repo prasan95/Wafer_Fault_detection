0ea6586a976d6fbde08d3bf8b17b5b4a764f0920
prashant <prasan.sp95@gmail.com>
2023-07-25 13:57:14 +0530

    First Commit

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,10 @@
+import sys
+import os
+import numpy as np
+import pandas as pd
+from pymongo import MongoClient
+from zipfile import Path
+from src.constant import *
+from src.exception import CustomException
+from src.logger import logging
+ 
